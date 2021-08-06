345b8cef674c19371553608e15a441fda7dd5d66
Luke Teal <l.teal21@gmail.com>
2021-08-05 21:55:48 -0400

    initial models

---
+++ server/models/Farm.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const FarmSchema = new Schema({
+  name: String,
+  description: String,
+  state: String,
+  town: String,
+  address: String,
+  zip: Number,
+  website: String,
+  items: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Item"
+    }
+  ]
+});
+
+const Farm = mongoose.model("Farm", FarmSchema);
+
+module.exports = Farm;
---
+++ server/models/Item.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    item: String,
+    price: Number,
+    unit: String,
+    count: Number, 
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+module.exports = Item;
