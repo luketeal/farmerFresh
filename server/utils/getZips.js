f2f6f127dddf683c28ba8eeb7b3993e665ce70d5
Luke Teal <l.teal21@gmail.com>
2021-08-10 20:43:25 -0400

    add try catch

--- server/utils/getZips.js
+++ server/utils/getZips.js
@@ -1,19 +1,23 @@
 const fetch = require("node-fetch");
-
+require('dotenv').config();
 
 module.exports = {
     getZips: async function ( zip ) {
         console.log(`here is the zipcode: ${zip}`)
         let requestURL = `https://redline-redline-zipcode.p.rapidapi.com/rest/radius.json/${zip}/10/mile`
         console.log(requestURL)
-        const response = await fetch(requestURL, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "e972b7d645mshf34d0fe56773d97p1fc220jsn1e1d00244010",
-                "x-rapidapi-host": "redline-redline-zipcode.p.rapidapi.com"
-            }
-        })
-        const data = await response.json()
+        try {
+            var response = await fetch(requestURL, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-key": "e972b7d645mshf34d0fe56773d97p1fc220jsn1e1d00244010",
+                    "x-rapidapi-host": "redline-redline-zipcode.p.rapidapi.com"
+                }
+            })        
+            var data = await response.json()
+        } catch(err) {
+            console.log(err)
+        }
         let zipArray = [];
         for (let each of data.zip_codes) {
             zipArray.push(each.zip_code)
