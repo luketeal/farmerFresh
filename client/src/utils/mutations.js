f44212f7d05a84553798397b54f9d790a9aed1f5
Luke Teal <l.teal21@gmail.com>
2021-08-12 17:01:00 -0400

    add queries and mutations JS files to frontend

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,106 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_FARM = gql`
+mutation addFarm(
+    $name: String!, 
+    $description: String, 
+    $state: String!, 
+    $town: String!, 
+    $address: String!, 
+    $website: String!, 
+    $zip:String!
+  ) {
+  addFarm(
+    name: $name
+  	description: $description
+    state: $state
+    town: $town
+    address: $address
+    website: $website
+    zip: $zip
+  ) {
+  _id
+  name
+  description
+  state
+  town
+  address
+  website
+  zip
+  }
+}
+`;
+
+export const CREATE_ITEM = gql`
+mutation addNewItem(
+    $name: String!, 
+    $price: String!, 
+    $count: String!, 
+    $unit: String!, 
+    $farmID: ID!, 
+  ) {
+  addItem(
+    name: $name
+  	price: $price
+    count: $count
+    unit: $unit
+    farmID: $farmID
+  ) {
+  _id
+  name
+  description
+  state
+  town
+  address
+  website
+  zip
+    items {
+      _id
+      name
+      price
+      count
+      unit
+    }
+  }
+}
+`;
+
+export const UPDATE_FARM = gql`
+mutation updateOneFarm(
+    $_id: ID!,
+  $name: String, 
+  $description: String, 
+  $state: String, 
+  $town: String, 
+  $address: String, 
+  $website: String, 
+  $zip:String
+) {
+updateFarm(
+  _id: $_id
+  name: $name
+    description: $description
+  state: $state
+  town: $town
+  address: $address
+  website: $website
+  zip: $zip
+) {
+_id
+name
+description
+state
+town
+address
+website
+zip
+items {
+  _id
+  name
+  price
+  count
+  unit
+}
+}
+}
+`;
