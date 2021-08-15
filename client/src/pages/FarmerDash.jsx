98eb90ee958dbfed538dde6e095e216363f3c9b8
kitclong <78049224+kitclong@users.noreply.github.com>
2021-08-15 15:28:28 -0400

    add additional page component

--- client/src/pages/FarmerDash.jsx
+++ client/src/pages/FarmerDash.jsx
@@ -1,9 +1,242 @@
-import React from 'react'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import FolderIcon from '@material-ui/icons/Folder';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+
+
+// Generating list items
+function generate(element) {
+    return [0, 1, 2, 3, 4, 5, 6].map((value) =>
+        React.cloneElement(element, {
+            key: value,
+        }),
+    );
+};
+
+
+const useStyles = makeStyles((theme) => ({
+
+    title: {
+        flexGrow: 1,
+        margin: theme.spacing(4, 0, 2),
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+    },
+    container: {
+        paddingTop: theme.spacing(4),
+        paddingBottom: theme.spacing(4),
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+        background: '#F50057',
+        borderRadius: 10,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '15px 45px',
+        boxShadow: '5 0 20px #eee',
+        marginTop: theme.spacing(3),
+    },
+    editIconSpace: {
+        marginRight: theme.spacing(5),
+    },
+}));
 
 export default function FarmerDash() {
+    const classes = useStyles();
+    const [dense, setDense] = React.useState(false);
+    const [secondary, setSecondary] = React.useState(false);
+
     return (
-        <div>
-            Something will def go here just wait
-        </div>
-    )
+        <Container component="main" maxWidth="md">
+
+            <CssBaseline />
+
+            <Typography component="h1" variant="h2">
+                Hi *username*
+            </Typography>
+
+            <div className={classes.appBarSpacer} />
+
+            <main className={classes.content}>
+                <Typography variant="h4">
+                    Register a farm below:
+                </Typography>
+
+                {/* Farm input field? */}
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="farmName"
+                                label="Farm Name"
+                                name="farmName"
+                                autoComplete="farmName"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="address"
+                                label="Address"
+                                name="address"
+                                autoComplete="address"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="contactInfo"
+                                label="Contact Info"
+                                name="contactInfo"
+                                autoComplete="contactInfo"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Register
+                    </Button>
+                </form>
+
+                <div className={classes.appBarSpacer} />
+
+                <Typography variant="h4">
+                    Enter your farm items:
+                </Typography>
+
+
+                {/* Veggie input field? */}
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="itemName"
+                                label="Item Name"
+                                name="itemName"
+                                autoComplete="itemName"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="description"
+                                label="Description"
+                                name="description"
+                                autoComplete="description"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="quantity"
+                                label="Quantity"
+                                name="quantity"
+                                autoComplete="quantity"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="whichFarm"
+                                label="Which Farm"
+                                name="whichFarm"
+                                autoComplete="whichFarm"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Submit Item
+                    </Button>
+                </form>
+
+
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={12}>
+                        <Typography variant="h4" className={classes.title}>
+                            Farm items and produce:
+                        </Typography>
+                        <div className={classes.demo}>
+                            <List dense={dense}>
+                                {generate(
+                                    <ListItem>
+                                        <ListItemAvatar>
+                                            <Avatar>
+                                                <FolderIcon />
+                                            </Avatar>
+                                        </ListItemAvatar>
+                                        <ListItemText
+                                            primary="Produce and veggie items"
+                                            secondary={secondary ? 'Secondary text' : null}
+                                        />
+                                        <ListItemSecondaryAction className={classes.editIconSpace}>
+                                            <IconButton edge="start" aria-label="edit" color="primary">
+                                                <EditIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+
+                                        <div className={classes.appBarSpacer} />
+
+                                        <ListItemSecondaryAction>
+                                            <IconButton edge="end" aria-label="delete">
+                                                <DeleteIcon />
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>,
+                                )}
+                            </List>
+                        </div>
+                    </Grid>
+                </Grid>
+            </main>
+        </Container >
+    );
 }
