205c34250e4c473ebdbb7e1dcf286f59001db29f
kitclong <78049224+kitclong@users.noreply.github.com>
2021-08-15 16:02:27 -0400

    update splashy

--- client/src/pages/Splash.jsx
+++ client/src/pages/Splash.jsx
@@ -9,14 +9,10 @@ import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import ExploreRoundedIcon from '@material-ui/icons/ExploreRounded';
-
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardHeader from '@material-ui/core/CardHeader';
-// import Grid from '@material-ui/core/Grid';
-// import StarIcon from '@material-ui/icons/StarBorder';
-
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -55,17 +51,25 @@ const useStyles = makeStyles((theme) => ({
     searchBar: {
         marginTop: theme.spacing(3),
     },
+    formControl: {
+        margin: theme.spacing(1),
+        display: 'flex',
+    },
 }));
 
 
 export default function Pricing() {
     const classes = useStyles();
 
+    const [zipcode, setZipcode] = React.useState('');
+
+    const handleChange = (event) => {
+        setZipcode(event.target.value);
+    };
+
     return (
         <React.Fragment>
             <CssBaseline />
-
-
             <Box className={classes.heroBox}>
                 <Avatar className={classes.avatar}>
                     <ExploreRoundedIcon />
@@ -81,9 +85,24 @@ export default function Pricing() {
                         Search for fresh produce in near you
                     </Typography>
 
-                    <div>
-                        <TextField id="outlined-basic" label="Enter zip code" variant="outlined" className={classes.searchBar} />
-                    </div>
+
+                    <FormControl variant="filled" className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-filled-label">Farms at these zipcodes</InputLabel>
+                        <Select
+                            labelId="select-filled-label"
+                            id="select-filled"
+                            value={zipcode}
+                            onChange={handleChange}
+                        >
+                            <MenuItem value="">
+                                <em>None</em>
+                            </MenuItem>
+                            <MenuItem value={10}>Superfarm</MenuItem>
+                            <MenuItem value={20}>Megafarm</MenuItem>
+                            <MenuItem value={30}>UltraFarm</MenuItem>
+                        </Select>
+                    </FormControl>
+
                     <div>
                         <Button href="./farmresults" variant="contained" color="neutral" className={classes.searchBtn} >
                             Search
@@ -92,10 +111,8 @@ export default function Pricing() {
 
                 </Container>
                 {/* End hero unit */}
-
             </Box>
-
-
         </React.Fragment>
     );
 }
+
