af1ea9c8003fce7e9f3a7ecb7cd605556f0acc8e
Luke Teal <l.teal21@gmail.com>
2021-08-15 20:53:46 -0400

    adding farm context

---
+++ client/src/utils/FarmContext.js
@@ -0,0 +1,33 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// Initialize new context for students
+const FarmContext = createContext();
+
+// We create a custom hook to provide immediate usage of the student context value (students) in other components
+export const useFarmContext = () => useContext(FarmContext);
+
+// The provider is responsible for holding our state, updating the state, and persisting values to the children
+export const FarmProvider = ({ children }) => {
+  const [search, setSearch] = useState("");
+
+  // Function to seat a student
+  const searchFarms = (searchState) => {
+    // Check if the user forgot to enter a name
+    if (!searchState) {
+      return;
+    }
+
+    // Update state with the search array with the newStudent
+    setSearch(searchState);
+  };
+
+  // The value prop expects an initial state object
+  return (
+    <FarmContext.Provider
+      value={{ search, searchFarms }}
+    >
+      {/* We render children in our component so that any descendent can access the value from the provider */}
+      {children}
+    </FarmContext.Provider>
+  );
+};
