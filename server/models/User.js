a152a1f1d2d995d21e0bbe741f765001f7ab9cf5
Luke Teal <l.teal21@gmail.com>
2021-08-17 15:39:53 -0400

    fix login mutation and user schema - add bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
@@ -22,6 +23,21 @@ const UserSchema = new Schema({
     }
   ]
 });
+
+// set up pre-save middleware to create password
+UserSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+UserSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 
 const User = mongoose.model("User", UserSchema);
