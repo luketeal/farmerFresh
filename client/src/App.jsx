9ae209699bcd3863f150b05fb41f7e1a99a210f4
Jfig27 <79497737+Jfig27@users.noreply.github.com>
2021-08-19 15:39:52 -0400

    Cart page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import {useState} from "react";
 import {useEffect} from 'react';
 import Auth from './utils/auth';
 import { setContext } from '@apollo/client/link/context';
+import Cart from './pages/Cart';
 
 
 
@@ -94,6 +95,9 @@ function App() {
             <Route exact path="/farmveggieresults">
               <FarmVeggieResults />
 
+            </Route>
+            <Route exact path="/cart">
+              <Cart/>
             </Route>
           
         </div>
