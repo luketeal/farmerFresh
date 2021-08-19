4ff8b14c61b1991dda7a1777e84f58735801eb64
Corey Nance <coreynance83@gmail.com>
2021-08-18 20:39:12 -0400

    creating server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+
+
+
+
+app.listen(PORT,()=> {
+    console.log(`App listening on PORT: ${PORT}`)
+})
+
+
+
